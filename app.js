8479bf2db7a69a8c312a942179c6363105e26312
carlossper <c_m_per@hotmail.com>
2018-06-14 15:25:17 +0100

    Login system done

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-var createError = require('http-errors');
+/*  EXPRESS SETUP  */
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+var exphbs = require('express-handlebars');
 var logger = require('morgan');
+var createError = require('http-errors');
+var passport = require('passport');
+var flash = require('connect-flash');
+var LocalStrategy = require('passport-local').Strategy;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+var User = require('./models/user');
 
 var app = express();
 
@@ -19,8 +26,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+ // app.use(express.cookieSession());
+
+//Connect Flash
+app.use(flash());
+ 
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+ 
+app.use(passport.initialize());
+app.use(passport.session());
+
+// routes
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.get("/secret",function(req, res){    
+     res.render("secret");
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -32,10 +58,12 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.user = req.user || null;
 
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+// GET users listing - test
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+// Login related routes
+
 
 module.exports = router;
